bee0166d09e907741d0b2e7826de92cf9b25e327
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-09-20 20:26:00 +0100

    firebase

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlBH7n4dnSUa48LITB1kd-S0ZQ7LfcPgg",
+    authDomain: "fireblogyt-bec2a.firebaseapp.com",
+    projectId: "fireblogyt-bec2a",
+    storageBucket: "fireblogyt-bec2a.appspot.com",
+    messagingSenderId: "788943269542",
+    appId: "1:788943269542:web:1b9c24f620ba202fecce67"
+  };
