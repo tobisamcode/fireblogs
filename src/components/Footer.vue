7b93f712ac2cc3bd3260fe41302a6f71ae469cde
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-05 22:50:48 +0100

    changes added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,7 +25,7 @@
                         <router-link class="link" :to="{name: 'Home' }">Home</router-link>
                         <router-link class="link" :to="{name: 'blogs' }">Blogs</router-link>
                         <router-link v-if="user" class="link" :to="{name: 'newpost' }">Create Post</router-link>
-                        <router-link v-if="!user" class="link login" :to="{name: 'login' }">Login In / Register</router-link>
+                        <router-link v-if="!user" class="link login" :to="{name: 'Login' }">Login In / Register</router-link>
                     </ul>
                 </div>
             </div>
